c375157779a840d75287ffd1e2c4b697de0a6fd7
Yamuna Rani R <yamunarani1610@gmail.com>
2026-09-04 22:07:48 +0530

    Implement integer reversal in solution.java

---
+++ Data Structures & Algorithms/Reverse integer/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int num = x%10;
+            if (rev > Integer.MAX_VALUE / 10 ||
+                (rev == Integer.MAX_VALUE / 10 && num > 7)) {
+                return 0;
+            }
+
+            if (rev < Integer.MIN_VALUE / 10 ||
+                (rev == Integer.MIN_VALUE / 10 && num < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + num;
+            x /= 10;
+        }
+        return rev;
+    }
+}
